544ccf2eef7d8667ae2b181d51eabc4a9c23df01
Kilarin <gith-dch00@sneakemail.com>
2022-04-25 17:42:23 -0500

    improved variable replacement logic

--- init.lua
+++ init.lua
@@ -7,6 +7,12 @@ simple_dialogs = { }
 local contextctr = {}
 local contextdlg = {}
 
+local chars = {}
+chars.tag="="
+chars.reply=">"
+chars.varopen="@["
+chars.varclose="]@"
+
 --[[
 local tag_filter=simple_dialogs.tag_filter
 local wrap=simple_dialogs.wrap
@@ -110,121 +116,6 @@ end --process_simple_dialog_control_fields
 
 
 
---this function will go through a string and build a list that tells what order
---to process parenthesis (or any other open close delimiter) in.
---example:
---12345678901234
---((3*(21+2))/4)
---list[1].open=5 close=10
---list[2].open=2 close=11
---list[3].open=1 close=14
---note that if you pass this a line that has bad syntax, it will not throw an error, but instead return an empty list
---list[].open and close are inclusive.  it includes the delimeter
---list[].opene and closee are exclusive.  it does NOT include the delimiter
---so in the above example:
---list[1].opene=6 close=9
---list[2].opene=3 close=10
---list[3].opene=2 close=13
-function simple_dialogs.build_grouping_list(line,opendelim,closedelim)
-	--minetest.log("GGG build grouping top, line="..line)
-	local grouping={}
-	grouping.list={}
-	grouping.origline=line
-	local openstack={}
-	local opendelim_len=string.len(opendelim)
-	grouping.opendelim_len=opendelim_len
-	local closedelim_len=string.len(closedelim)
-	grouping.closedelim_len=closedelim_len
-	--local i=string.find(line,opendelim)-1 --start just before first open delim   (causes problems because of [ being a special character to find)
-	for i=1,string.len(line),1 do
-		if string.sub(line,i,i+opendelim_len-1)==opendelim then --open delim
-			openstack[#openstack+1]=i  --open pos onto stack.
-		elseif string.sub(line,i,i+closedelim_len-1)==closedelim then -- close delim
-			--if you find parens out of order, just stop and return an empty list
-			if #openstack<1 then return {} end 
-			local l=#grouping.list+1
-			grouping.list[l]={}
-			local gll=grouping.list[l]
-			gll.open=openstack[#openstack]
-			gll.opene=gll.open+(opendelim_len)
-			gll.close=i+(closedelim_len-1)
-			gll.closee=i-1
-			gll.section=string.sub(grouping.origline,gll.open,gll.close)
-			gll.sectione=string.sub(grouping.origline,gll.opene,gll.closee)
-			table.remove(openstack,#openstack) --remove from stack
-		end --if
-	end --while
-	--minetest.log("GGG about to return")
-	return grouping
-end --build_grouping_list
-
-
-
-function simple_dialogs.grouping_replace(grouping,idx,replacewith,incl_excl)
-	minetest.log("GGGR top grouping="..dump(grouping).." idx="..idx.." replacewith="..replacewith.." incl_excl="..incl_excl)
-	if not incl_excl then incl_excl="INCLUSIVE" end
-	if not grouping.line then grouping.line=grouping.origline end
-	local s=grouping.list[idx].open
-	local e=grouping.list[idx].close
-	if incl_excl=="EXCLUSIVE" then 
-		s=grouping.list[idx].opene
-		e=grouping.list[idx].closee
-	end 
-	local origlen=e-s+1
-	local diff=string.len(replacewith)-origlen
-	local line=grouping.line
-	grouping.line=string.sub(line,1,s-1)..replacewith..string.sub(line,e+1)
-	for i=1,#grouping.list,1 do
-		local gli=grouping.list[i]
-		if gli.open>s then gli.open=gli.open+diff end
-		if gli.opene>s then gli.opene=gli.opene+diff end
-		if gli.close>s then gli.close=gli.close+diff end
-		if gli.closee>s then gli.closee=gli.closee+diff end
-	end --for
-	minetest.log("GGGR bot grouping="..dump(grouping))
-return grouping.line
-end--grouping_replace
-
-
-
-function simple_dialogs.upper_case_vars(line)
-	local grouping=simple_dialogs.build_grouping_list(line,"@[","]@")
-	for i=1,#grouping.list,1 do
-		--I could run this through the varname filter, but do I really want to do that?
-		line=simple_dialogs.grouping_replace(grouping,i,string.upper(grouping.list[i].sectione),"EXCLUSIVE")
-		--line=string.sub(line,1,list[i].open-1)..string.upper(string.sub(line,list[i].open,list[i].close))..string.sub(line,list[i].close+1)
-	end --for
-	return line
-end --upper_case_vars
-
-
-
-function simple_dialogs.load_dialog_var(npcself,varname,varval)
-	if npcself then
-		if not npcself.dialogvars then npcself.dialogvars = {} end
-		npcself.dialogvars[simple_dialogs.varname_filter(varname)] = varval
-	end
-end --load_dialog_var
-
-
-
---this function populates variables within dialog text
-function simple_dialogs.populate_vars(npcself,line)
-	if npcself and npcself.dialogvars then
-		local grouping=simple_dialogs.build_grouping_list(line,"@[","]@")
-		for i=1,#grouping.list,1 do
-			local gli=grouping.list[i]
-			local k=gli.sectione
-			if npcself.dialogvars[gli.sectione] then
-				line=simple_dialogs.grouping_replace(grouping,i,npcself.dialogvars[gli.sectione],"INCLUSIVE")
-			--line=string.sub(line,1,list[i].open-1)..string.upper(string.sub(line,list[i].open,list[i].close))..string.sub(line,list[i].close+1)
-			end --if
-		end --for
-	end --if
-	return line
-end --populate_vars
-
-
 
 
 --[[
@@ -253,7 +144,7 @@ function simple_dialogs.load_dialog_from_string(npcself,dialogstr,pname)
 	npcself.dialog = {}
 	npcself.dialogvars = {}
 	--add playername to variables IF it was passed in
-	if pname then simple_dialogs.load_dialog_var(npcself,"PLAYERNAME",pname) end  
+	if pname then simple_dialogs.load_dialog_var(npcself,"PLAYERNAME",pname) end
 	local tag = ""
 	local tagcount=1
 	local weight=1
@@ -262,7 +153,8 @@ function simple_dialogs.load_dialog_from_string(npcself,dialogstr,pname)
 	local reply = ""
 	for line in dialogstr:gmatch '[^\n]+' do
 		--minetest.log("simple_dialogs->loadstr: line="..line)
-		if string.sub(line,1,1) == "=" then  --we found a tag, process it
+		local firstchar=string.sub(line,1,1)
+		if firstchar == chars.tag then  --we found a tag, process it
 			tag=line  --this might still include weight
 			--get the weight from parenthesis
 			weight=1
@@ -293,7 +185,7 @@ function simple_dialogs.load_dialog_from_string(npcself,dialogstr,pname)
 			npcself.dialog[tag][tagcount]={}
 			npcself.dialog[tag][tagcount].weight=weight
 			npcself.dialog[tag][tagcount].reply={}
-		elseif string.sub(line,1,1) == ">" and tag ~= "" then  --we found a reply, process it
+		elseif firstchar == chars.reply and tag ~= "" then  --we found a reply, process it
 			--if we got a reply, then the say is ended, add it
 			npcself.dialog[tag][tagcount].say=say
 			--split into target and reply
@@ -310,9 +202,25 @@ function simple_dialogs.load_dialog_from_string(npcself,dialogstr,pname)
 			replycount=replycount+1
 		--we check that a tag is set to avoid errors, just in case they put text before the first tag
 		--we check that replycount=1 because we are going to ignore any text between the replies and the next tag
+		elseif firstchar==":" then --commands
+			local spc=string.find(line," ",2)
+			if spc then
+				local cmnd=string.upper(string.sub(line,2,spc-1))
+				local str=string.sub(line,spc+1) --rest of line without the command
+				str=simple_dialogs.populate_vars(npcself,str) --populate any variables
+				if cmnd=="SET" then
+					local eq=string.find(str,"=",6)
+					if eq then
+						local k=string.sub(str,1,eq-1)
+						local v=string.sub(str,eq+1)
+						if v then
+							k=simple_dialogs.varname_filter(k)
+							simple_dialogs.load_dialog_var(npcself,k,v) 
+						end --if v
+					end --if eq
+				end --if cmnd
+			end --if spc
 		elseif tag~="" and replycount==1 then  --we found a dialog line, process it
-			--first fix any variables by changing them to upper case
-			line=simple_dialogs.upper_case_vars(line)
 			say=say..line.."\n"
 		end
 	end --for line in dialog
@@ -373,8 +281,9 @@ end --dialog_help
 ----------------------------------------------------------------------
 
 
---call with tag=START for starting a dialog
+--call with tag=START for starting a dialog, or with no tag
 function simple_dialogs.show_dialog_formspec(pname,npcself,tag)
+	if not tag then tag="START" end
 	minetest.show_formspec(pname,"simple_dialogs:dialog",get_dialog_formspec(pname,npcself,tag))
 end --show_dialog_formspec
 
@@ -384,6 +293,7 @@ function simple_dialogs.get_dialog_formspec(pname,npcself,tag)
 	--minetest.log("simple_dialogs->getdialogformspec: npcself="..dump(npcself))
 	contextdlg[pname]={}
 	contextdlg[pname].npcId=simple_dialogs.set_npc_id(npcself) --store the npc id in local context so we can use it when the form is returned.  (cant store self)
+	---minetest.log("FFF setting contextdlg[pname] contextdlg="..dump(contextdlg))
 	local formspec={
 		"formspec_version[4]",
 		"size[10,11.375]", 
@@ -475,8 +385,10 @@ end
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	local pname = player:get_player_name()
-	if formname ~= "simple_dialogs:dialog" then  
-		if contextdlg[name] then contextdlg[name]=nil end
+	if formname ~= "simple_dialogs:dialog" then
+		--if contextdlg[pname] then contextdlg[pname]=nil end  
+		--can NOT clear context here because this can be called from inside the control panel, 
+		--and that can be from a DIFFERENT mod where I can not predict the name
 		return 
 	end
 	--minetest.log("simple_dialogs->receive_fields dialog: fields="..dump(fields))
@@ -518,6 +430,44 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 end) --register_on_player_receive_fields dialog
 
 
+--------------------------------------------------------------
+
+
+
+function simple_dialogs.load_dialog_var(npcself,varname,varval)
+	if npcself then
+		if not npcself.dialogvars then npcself.dialogvars = {} end
+		npcself.dialogvars[simple_dialogs.varname_filter(varname)] = varval
+	end
+end --load_dialog_var
+
+
+
+--this function populates variables within dialog text
+function simple_dialogs.populate_vars(npcself,line)
+	if npcself and npcself.dialogvars then
+		local grouping=simple_dialogs.build_grouping_list(line,chars.varopen,chars.varclose)
+		--minetest.log("CCC vars="..dump(npcself.dialogvars))
+		for i=1,#grouping.list,1 do
+			--local gli=grouping.list[i]
+			--minetest.log("CCC beforesectione i="..i.." grouping="..dump(grouping))
+			local sectione=simple_dialogs.grouping_section(grouping,i,"EXCLUSIVE") --get section from string
+			local k=simple_dialogs.varname_filter(sectione)  --k is our key value
+			--minetest.log("CCC i="..i.." sectione="..sectione.." k="..k)
+			if npcself.dialogvars[k] then --is this if necessary?
+				line=simple_dialogs.grouping_replace(grouping,i,npcself.dialogvars[k],"INCLUSIVE")
+			--line=string.sub(line,1,list[i].open-1)..string.upper(string.sub(line,list[i].open,list[i].close))..string.sub(line,list[i].close+1)
+			end --if
+		end --for
+	end --if
+	return line
+end --populate_vars
+
+
+
+
+--------------------------------------------------------------
+
 
 function simple_dialogs.get_npcself_from_id(npcId)
 	if npcId==nil then return nil
@@ -558,5 +508,118 @@ function dump(o)
 		return tostring(o)
 	end
 end
+
+
+
+
+--[[ *******************************************************************************
+Grouping
+These would probably be better separated into a different lua, perhaps even a different mod?
+--]]
+
+
+
+
+
+--this function will go through a string and build a list that tells what order
+--to process parenthesis (or any other open close delimiter) in.
+--example:
+--12345678901234
+--((3*(21+2))/4)
+--list[1].open=5 close=10
+--list[2].open=2 close=11
+--list[3].open=1 close=14
+--note that if you pass this txt that has bad syntax, it will not throw an error, but instead return an empty list
+--list[].open and close are inclusive.  it includes the delimeter
+--list[].opene and closee are exclusive.  it does NOT include the delimiter
+--so in the above example:
+--list[1].opene=6 close=9
+--list[2].opene=3 close=10
+--list[3].opene=2 close=13
+function simple_dialogs.build_grouping_list(txt,opendelim,closedelim)
+	--minetest.log("GGG build grouping top, txt="..txt)
+	local grouping={}
+	grouping.list={}
+	grouping.origtxt=txt --is this useful?
+	grouping.txt=txt
+	local openstack={}
+	local opendelim_len=string.len(opendelim)
+	grouping.opendelim_len=opendelim_len
+	local closedelim_len=string.len(closedelim)
+	grouping.closedelim_len=closedelim_len
+	--local i=string.find(txt,opendelim)-1 --start just before first open delim   (causes problems because of [ being a special character to find)
+	for i=1,string.len(txt),1 do
+		if string.sub(txt,i,i+opendelim_len-1)==opendelim then --open delim
+			openstack[#openstack+1]=i  --open pos onto stack.
+		elseif string.sub(txt,i,i+closedelim_len-1)==closedelim then -- close delim
+			--if you find parens out of order, just stop and return an empty list
+			if #openstack<1 then return {} end 
+			local l=#grouping.list+1
+			grouping.list[l]={}
+			local gll=grouping.list[l]
+			gll.open=openstack[#openstack]
+			gll.opene=gll.open+(opendelim_len)
+			gll.close=i+(closedelim_len-1)
+			gll.closee=i-1
+			--gll.section=string.sub(grouping.origtxt,gll.open,gll.close)
+			--gll.sectione=string.sub(grouping.origtxt,gll.opene,gll.closee)
+			table.remove(openstack,#openstack) --remove from stack
+		end --if
+	end --while
+	--minetest.log("GGG about to return")
+	return grouping
+end --build_grouping_list
+
+
+
+function simple_dialogs.grouping_section(grouping,i,incl_excl)
+	if not incl_excl then incl_excl="INCLUSIVE" end
+	--minetest.log("GGGs top i="..i.." incl_excl="..incl_excl)
+	local gli=grouping.list[i]
+	--minetest.log("GGGs after gli")
+	if incl_excl=="INCLUSIVE" then
+		--minetest.log("GGGs inclusive")
+		return string.sub(grouping.txt,gli.open,gli.close)
+	else
+		--minetest.log("GGGs exclusive") 
+		return string.sub(grouping.txt,gli.opene,gli.closee)
+	end
+end --grouping_section
+
+
+
+function simple_dialogs.grouping_sectione(grouping,i)
+	--minetest.log("GGGse i="..i.." grouping="..dump(grouping))
+	simple_dialogs.grouping_section(grouping,i,"EXCLUSIVE")
+end --grouping_sectione
+
+
+
+
+function simple_dialogs.grouping_replace(grouping,idx,replacewith,incl_excl)
+	--minetest.log("***GGGR top grouping="..dump(grouping).." idx="..idx.." replacewith="..replacewith.." incl_excl="..incl_excl)
+	if not incl_excl then incl_excl="INCLUSIVE" end
+	local s=grouping.list[idx].open
+	local e=grouping.list[idx].close
+	if incl_excl=="EXCLUSIVE" then 
+		s=grouping.list[idx].opene
+		e=grouping.list[idx].closee
+	end 
+	local origlen=e-s+1
+	local diff=string.len(replacewith)-origlen
+	local txt=grouping.txt
+	grouping.txt=string.sub(txt,1,s-1)..replacewith..string.sub(txt,e+1)
+	for i=1,#grouping.list,1 do
+		local gli=grouping.list[i]
+		if gli.open>s then gli.open=gli.open+diff end
+		if gli.opene>s then gli.opene=gli.opene+diff end
+		if gli.close>s then gli.close=gli.close+diff end
+		if gli.closee>s then gli.closee=gli.closee+diff end
+	end --for
+	--minetest.log("GGGR bot grouping="..dump(grouping))
+	--minetest.log("GGGR2 bot origtxt="..grouping.origtxt)
+	--minetest.log("GGGR2 bot     txt="..grouping.txt)
+return grouping.txt
+end--grouping_replace
 
 
